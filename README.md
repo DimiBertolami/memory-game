b6fb8d9256507567783185cf2693a520d8ae0f32
Dimitri Bertolami <dimibertolami@gmail.com>
2022-05-20 11:02:32 +0200

    first draft with minor functionality

--- README.md
+++ README.md
@@ -2,10 +2,14 @@
 everyone knows it, everybody has played it
 
 ## Must-have features
-1. [ ] Clicking cards turns them around
-1. [ ] Randomly position the cards
-1. [x] Readme, but that shouldn't even have to be said
-1. [ ] Explanation on the page itself
+1. Clicking cards turns them around 
+[x] done
+2. Randomly position the cards
+[ ] Next on my list
+3. Readme, but that shouldn't even have to be said
+[x] Done
+4. Explanation on the page itself
+[ ]
 
 ## Nice-to-have features
 1. [ ] Make it playable by keyboard
---
+++ game.js
@@ -0,0 +1,90 @@
+function turnOne(){
+    turnCard("One");
+}
+function turnTwo(){
+    turnCard("Two");
+}
+function turnThree(){
+    turnCard("Three");
+}
+function turnFour(){
+    turnCard("Four");
+}
+function turnFive(){
+    turnCard("Five");
+}
+function turnSix(){
+    turnCard("Six");
+}
+function turnSeven(){
+    turnCard("Seven");
+}
+function turnEight(){
+    turnCard("Eight");
+}
+function turnNine(){
+    turnCard("Nine");
+}
+function turnTen(){
+    turnCard("Ten");
+}
+function turnEleven(){
+    turnCard("Eleven");
+}
+function turnTwelve(){
+    turnCard("Twelve");
+}
+function turnThirteen(){
+    turnCard("Thirteen");
+}
+function turnFourteen(){
+    turnCard("Fourteen");
+}
+function turnFifteen(){
+    turnCard("Fifteen");
+}
+function turnSixteen(){
+    turnCard("Sixteen");
+}
+function turnSeventeen(){
+    turnCard("Seventeen");
+}
+function turnEighteen(){
+    turnCard("Eighteen");
+}
+function turnNineteen(){
+    turnCard("Nineteen");
+}
+function turnTwenty(){
+    turnCard("Twenty");
+}
+function turnTwentyOne(){
+    turnCard("TwentyOne");
+}
+function turnTwentyTwo(){
+    turnCard("TwentyTwo");
+}
+function turnTwentyThree(){
+    turnCard("TwentyThree");
+}
+function turnTwentyFour(){
+    turnCard("TwentyFour");
+}
+function turnTwentyFive(){
+    turnCard("TwentyFive");
+}
+function turnTwentySix(){
+    turnCard("TwentySix");
+}
+function turnTwentySeven(){
+    turnCard("TwentySeven");
+}
+function turnTwentyEight(){
+    turnCard("TwentyEight");
+}
+
+function turnCard(card){
+
+    let img = document.getElementById(card);
+    img.src = "images/cardback.jpg"
+}
